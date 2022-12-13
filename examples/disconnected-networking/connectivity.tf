5e46114ffb29997a43a03063f796f346889d3d59
Paul Revello <paul@revello.dev>
2022-12-13 14:47:29 -0500

    Update connectivity.tf

--- examples/disconnected-networking/connectivity.tf
+++ examples/disconnected-networking/connectivity.tf
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 // Setup the Connectivity project that hosts the service
 // attachements that clients consume
 resource "google_project" "connectivityProject" {
@@ -35,21 +50,6 @@ resource "google_compute_ha_vpn_gateway" "connectivityGateway" {
   name    = "connectivity-gateway"
   network = google_compute_network.connectivityVPC.self_link
 }
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 // Create a Cloud Router
 resource "google_compute_router" "connectivityRouter" {
   project = google_project.connectivityProject.number
